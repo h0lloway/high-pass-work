4013622a0bab7f27934c99f0948c3da1d339873f
h0lloway <gvozdb27@gmail.com>
2022-01-07 15:14:33 +0300

    add "high pass"

---
+++ src/js/main.js
@@ -0,0 +1,50 @@
+// burger menu
+
+$(document).ready(function () {
+  $('.bottom__burger').click(function (event) {
+    $('.bottom__burger,.bottom__nav').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
+
+// yandex map
+
+ymaps.ready(init);
+function init() {
+  const mapElem = document.querySelector('#map');
+  const myMap = new ymaps.Map(
+    "map",
+    {
+      center: [55.75846306898368, 37.601079499999905],
+      zoom: 14,
+      controls: ['geolocationControl', 'zoomControl']
+    },
+    {
+      suppressMapOpenBlock: true,
+      geolocationControlSize: "large",
+      geolocationControlPosition: { top: "200px", right: "20px" },
+      geolocationControlFloat: 'none',
+      zoomControlSize: "small",
+      zoomControlFloat: "none",
+      zoomControlPosition: { top: "120px", right: "20px" }
+    }
+  );
+
+  const myPlacemark = new ymaps.Placemark(
+    [55.75846306898368, 37.601079499999905],
+    {},
+    {
+      iconLayout: "default#image",
+      iconImageHref: "img/location.svg",
+      iconImageSize: [20, 20],
+      iconImageOffset: [-20, -40],
+    }
+  );
+
+  myMap.geoObjects.add(myPlacemark);
+
+  setTimeout(() => {
+    myMap.container.fitToViewport();
+  }, 5000);
+}
+
